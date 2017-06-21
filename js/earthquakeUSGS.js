d8d45e5969614668c05a098a810c8e804d2c0079
e.jager@amsterdam.nl <info@lytrix.com>
2017-06-21 02:17:47 +0200

    added cors hack

--- js/earthquakeUSGS.js
+++ js/earthquakeUSGS.js
@@ -36,10 +36,12 @@
 
     // Download the data
     myConnector.getData = function(table, doneCallback) {
-        $.ajax(xhrFields: {
+        $.ajax({
+        url:"http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson",
+        xhrFields: {
           withCredentials: true
-        },
-        url:"http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson"}.done(function(resp) {
+          }
+        }).done(function(resp) {
             var feat = resp.features,
                 tableData = [];
 
