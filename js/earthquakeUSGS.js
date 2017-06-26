8753aa09dd813fbed1821b81ea5c36c368c90593
e.jager@amsterdam.nl <info@lytrix.com>
2017-06-26 13:28:11 +0200

    added gebieden to index.html

--- js/earthquakeUSGS.js
+++ js/earthquakeUSGS.js
@@ -35,7 +35,7 @@
     };
 
     // Download the data
-    myConnector.getData = function(table, doneCallback) {
+    myConnector.getData = function(table) {
         $.ajax({
         url:"http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson",
         xhrFields: {
@@ -57,7 +57,7 @@
             }
 
             table.appendRows(tableData);
-            doneCallback();
+           
         });
     };
 
